635330a86c06ad209aa03a3bc1aba5a0fdfd8bf8
Michel Rasquin <michel.rasquin@colorado.edu>
2013-08-13 19:24:25 -0600

    Script to evaluate the element and node imbalance of a partitioned mesh

---
+++ Scripts-CountEntity/check-imbalance.sh
@@ -0,0 +1,25 @@
+# Give the name of the proc case dir
+nparts=$1
+nargs=1
+
+echo 'This script evaluates the mesh imbalance in terms of elements and nodes'
+echo 'Notes: this script reads numElm*.dat and numNod*.dat produced by one of the count-* scripts'
+
+function die() {
+echo -e "${1}"
+exit 1
+}
+
+# check that user entered proper number of args
+if [[ "${#}" -ne "$nargs" ]]
+then
+die "\n Check usage: \n  $0\n  <numpart>\n"
+fi
+
+#Get the number of parts from ##-procs_case dir
+
+fileElm=numElm_$nparts.dat
+fileNod=numNod_$nparts.dat
+
+cat $fileElm | awk '{if($1>max) max=$1 ; sum+=$1} END {print "Number of parts:",NR, "- Max elm:",max, "- Avg elm:",sum/NR, "Elm imb=", max/sum*NR}'
+cat $fileNod | awk '{if($1>max) max=$1 ; sum+=$1} END {print "Number of parts:",NR, "- Max nod:",max, "- Avg nod:",sum/NR, "Nod imb=", max/sum*NR}'

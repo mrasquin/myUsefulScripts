760ccceab9b8356a91686238f88bf2c25ae73904
Michel Rasquin <michel.rasquin@colorado.edu>
2014-04-10 21:24:07 -0600

    GeomSim model now supported

--- BLMesher/runBLMesherParallel.sh
+++ BLMesher/runBLMesherParallel.sh
@@ -8,8 +8,8 @@ print_usage()
    echo " <X>: geometric model"
    echo " <Y>: attribute file"
    echo " <Z>: number of processors"
-   echo " Note: soft add +simmodsuite-9.0-130715 if not already done"
-   echo " Note: soft add +openmpi-gnu-1.6.5 if not already done"
+   echo " Note: soft add +simmodsuite-9.0-140403 if not already done"
+   echo " Note: soft add +openmpi-gnu482-1.6.5-thread if not already done"
 }  
 
 if [ "$#" -ne "$nargs" ] ; then
@@ -25,15 +25,15 @@ echo
 echo "Running parallel BL mesher (make sure to check BLMesher.log)..."
 
 if [[ $modelfile == *.x_t ]] || [[ $modelfile == *.xmt_txt ]] || [[ $modelfile == *.X_T ]] || [[ $modelfile == *XMT_TXT ]] ; then
-	executable=BLMesher-openmpi-O-parasolid-9.0-130805beta
+	executable=BLMesher-openmpi-O-parasolid-9.0-140403beta
 elif [[ $modelfile == *.sat ]] || [[ $modelfile == *.SAT ]] ; then
-	executable=BLMesher-openmpi-O-acis-9.0-130805beta
+	executable=BLMesher-openmpi-O-acis-9.0-140403beta
+elif [[ $modelfile == *.smd ]] || [[ $modelfile == *.SMD ]] ; then
+	executable=BLMesher-openmpi-O-geomsim-9.0-140403beta
 else
 	echo File extension unknown for $modelfile
 	exit 1
 fi
 
 mpirun -np $nprocs /users/mrasquin/develop/Meshing/BLMesherParallel/bin/x86_64_linux/$executable $modelfile mesh.sms 2 1.0 $attribfile > BLMesher.log 2>&1
-
-
 

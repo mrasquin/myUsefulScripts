f9d82d45d1ff4066f3adb1abe8e8bdb3b457534b
Michel Rasquin <michel.rasquin@colorado.edu>
2014-09-15 15:34:08 -0600

    Simmetrix library update

--- BLMesher/runBLMesherParallel.sh
+++ BLMesher/runBLMesherParallel.sh
@@ -8,7 +8,7 @@ print_usage()
    echo " <X>: geometric model"
    echo " <Y>: attribute file"
    echo " <Z>: number of processors"
-   echo " Note: soft add +simmodsuite-9.0-140403 if not already done"
+   echo " Note: soft add +simmodsuite-9.0-140630 if not already done"
    echo " Note: soft add +openmpi-gnu482-1.6.5-thread if not already done"
 }  
 
@@ -25,11 +25,11 @@ echo
 echo "Running parallel BL mesher (make sure to check BLMesher.log)..."
 
 if [[ $modelfile == *.x_t ]] || [[ $modelfile == *.xmt_txt ]] || [[ $modelfile == *.X_T ]] || [[ $modelfile == *XMT_TXT ]] ; then
-	executable=BLMesher-openmpi-O-parasolid-9.0-140403beta
+	executable=BLMesher-openmpi-O-parasolid-9.0-140724
 elif [[ $modelfile == *.sat ]] || [[ $modelfile == *.SAT ]] ; then
-	executable=BLMesher-openmpi-O-acis-9.0-140403beta
+	executable=missing
 elif [[ $modelfile == *.smd ]] || [[ $modelfile == *.SMD ]] ; then
-	executable=BLMesher-openmpi-O-geomsim-9.0-140403beta
+	executable=BLMesher-openmpi-O-geomsim-9.0-140724
 else
 	echo File extension unknown for $modelfile
 	exit 1
--- BLMesher/runBLMesherSerial.sh
+++ BLMesher/runBLMesherSerial.sh
@@ -7,7 +7,7 @@ print_usage()
    echo "Usage: $0 <X> <Y>"
    echo " <X>: geometric model"
    echo " <Y>: attribute file"
-   echo " Note: soft add +simmodsuite-9.0-130715 if not already done"
+   echo " Note: soft add +simmodsuite-9.0-140630 if not already done"
 }  
 
 if [ "$#" -ne "$nargs" ] ; then
@@ -19,19 +19,18 @@ modelfile=$1
 attribfile=$2
 
 echo
-echo "Running BL mesher (make sure to check $logfile)..."
+echo "Running Serial BL mesher (make sure to check BLMesher.log)..."
 
 if [[ $modelfile == *.x_t ]] || [[ $modelfile == *.xmt_txt ]] || [[ $modelfile == *.X_T ]] || [[ $modelfile == *XMT_TXT ]] ; then
-	#executable=BLMesher-O-parasolid-9.0-130715
-	executable=BLMesher-O-parasolid-9.0-130805beta
+	executable=BLMesher-O-parasolid-9.0-140724
 elif [[ $modelfile == *.sat ]] || [[ $modelfile == *.SAT ]] ; then
-	#executable=BLMesher-O-acis-9.0-130715
-	executable=BLMesher-O-acis-9.0-130805beta
+	executable=missing
+elif [[ $modelfile == *.smd ]] || [[ $modelfile == *.SMD ]] ; then
+	executable=BLMesher-O-geomsim-9.0-140724
 else
 	echo File extension unknown for $modelfile
 	exit 1
 fi
 
 /users/mrasquin/develop/Meshing/BLMesher/bin/x86_64_linux/$executable $modelfile geom_ver63.sms 2 1.0 $attribfile > BLMesher.log 2>&1
-
 
